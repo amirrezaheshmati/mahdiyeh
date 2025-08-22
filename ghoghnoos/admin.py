895b3b7b95015b6d2345c2043cd9b7b827565a99
amirreza <hshmtyamyrrda631@gmail.com>
2025-08-22 08:36:17 +0330

    add product

--- ghoghnoos/admin.py
+++ ghoghnoos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Product , Subset , Category
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Subset)
+admin.site.register(Product)
--- ghoghnoos/models.py
+++ ghoghnoos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+#from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model) :
+    name = models.CharField(max_length=20)
+    
+    def __str__(self) :
+        return self.name
+
+class Subset(models.Model) :
+    category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    
+    def __str__(self) :
+        return self.name
+
+class Product(models.Model):
+    subset = models.ForeignKey(Subset , on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    price = models.PositiveBigIntegerField()
+    #likes = models.ManyToManyField(User , blank=True , related_name="like")
+    picture = models.ImageField()
+    
+    def __str__(self) :
+        return self.name
